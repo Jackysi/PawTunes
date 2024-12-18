f42c4c6b257cf185c504724f80279892818f03a2
Jaka Prašnikar <jacky@prahec.com>
2024-12-18 17:59:40 +0100

    Fixed: small issue with PHP 8.4, which is strict with null types now

--- inc/lib/PawException.php
+++ inc/lib/PawException.php
@@ -17,7 +17,7 @@
 
     final class PawException extends \RuntimeException {
 
-        public function __construct( $message, $code = 0, Exception $previous = null ) {
+        public function __construct( $message, $code = 0, ?Exception $previous = null ) {
 
             parent::__construct( $message, $code, $previous );
 
